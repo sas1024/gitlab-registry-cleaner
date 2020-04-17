e48abebb4208781c592116af49c389dd4be7128a
Alexander Korelskiy <korelskiy@uteka.ru>
2020-04-17 16:12:13 +0300

    Rewrite for ReviewApps images support

--- main.go
+++ main.go
@@ -13,10 +13,12 @@ import (
 )
 
 type Config struct {
-	GitlabBaseUrl     string
-	AuthToken         string
-	ExcludedTags      []string
-	SaveRevisionCount int
+	GitlabBaseUrl       string
+	AuthToken           string
+	ExcludedTags        []string
+	ReviewAppsTags      []string
+	SaveRevisionCount   int
+	SaveReviewAppsCount int
 }
 
 var (
@@ -53,6 +55,11 @@ func main() {
 	}
 
 	for _, p := range projects {
+		//if p.NameWithNamespace != "uteka / desktop" {
+		//	continue
+		//}
+
+		log.Println(p.NameWithNamespace)
 
 		repositories, _, err := git.ContainerRegistry.ListRegistryRepositories(p.ID, nil)
 		if err != nil {
@@ -61,7 +68,7 @@ func main() {
 		}
 
 		for _, r := range repositories {
-			tt, _, err := git.ContainerRegistry.ListRegistryRepositoryTags(p.ID, r.ID, nil)
+			tt, _, err := git.ContainerRegistry.ListRegistryRepositoryTags(p.ID, r.ID, &gitlab.ListRegistryRepositoryTagsOptions{PerPage: 200})
 			if err != nil {
 				die(err)
 			}
@@ -75,62 +82,70 @@ func main() {
 				tags = append(tags, tag)
 			}
 
-			excludedRevisions := make(map[string]bool)
+			var reviewAppTags TagsList
+			var excludedTags TagsList
+			var uncategorizedTags TagsList
 
 			for _, t := range tags {
-				for _, e := range cfg.ExcludedTags {
-					found, _ := regexp.MatchString(e, t.Name)
+				isIgnoredTag := false
+				for _, r := range cfg.ReviewAppsTags {
+					found, _ := regexp.MatchString(r, t.Name)
 					if !found {
 						continue
 					}
-					excludedRevisions[t.ShortRevision] = true
+					reviewAppTags = append(reviewAppTags, t)
+					isIgnoredTag = true
+				}
+				for _, r := range cfg.ExcludedTags {
+					found, _ := regexp.MatchString(r, t.Name)
+					if !found {
+						continue
+					}
+					excludedTags = append(excludedTags, t)
+					isIgnoredTag = true
+				}
+				if !isIgnoredTag {
+					uncategorizedTags = append(uncategorizedTags, t)
 				}
 			}
+			sort.Sort(reviewAppTags)
+			sort.Sort(uncategorizedTags)
 
-			tags = filter(tags, cfg.ExcludedTags)
+			var ignoredTags TagsList // will be ignored from deletion
 
-			var filteredTags TagsList
+			// check uncategorized tags limits
+			if len(uncategorizedTags) > cfg.SaveRevisionCount {
+				ignoredTags = uncategorizedTags[len(uncategorizedTags)-cfg.SaveRevisionCount:]
+			} else {
+				ignoredTags = uncategorizedTags
+			}
+
+			// check review apps tags limits
+			if len(reviewAppTags) > cfg.SaveReviewAppsCount {
+				ignoredTags = append(ignoredTags, reviewAppTags[len(reviewAppTags)-cfg.SaveReviewAppsCount:]...)
+			} else {
+				ignoredTags = append(ignoredTags, reviewAppTags...)
+			}
+
+			ignoredTags = append(ignoredTags, excludedTags...)
+
+			ignoredShortRevisions := make(map[string]struct{})
+			for _, r := range ignoredTags {
+				ignoredShortRevisions[r.ShortRevision] = struct{}{}
+			}
+
 			for _, t := range tags {
-				if _, ok := excludedRevisions[t.ShortRevision]; !ok {
-					filteredTags = append(filteredTags, t)
+				if _, ok := ignoredShortRevisions[t.ShortRevision]; ok {
+					continue
 				}
-			}
-
-			sort.Sort(filteredTags)
-
-			if len(filteredTags) <= cfg.SaveRevisionCount {
-				// skip registry tags deletion
-				continue
-			}
-
-			var deleteTags TagsList
-			for _, t := range filteredTags[:len(filteredTags)-cfg.SaveRevisionCount] {
-				deleteTags = append(deleteTags, t)
-			}
-
-			for _, t := range deleteTags {
 				log.Println(fmt.Sprintf("Delete %s", t.Location))
 				_, err := git.ContainerRegistry.DeleteRegistryRepositoryTag(p.ID, r.ID, t.Name)
 				if err != nil {
 					die(err)
 				}
-
 			}
 		}
 	}
-}
-
-func filter(source []*gitlab.RegistryRepositoryTag, filter []string) (filtered []*gitlab.RegistryRepositoryTag) {
-	for _, t := range source {
-		found := false
-		for _, f := range filter {
-			found, _ = regexp.MatchString(f, t.Name)
-		}
-		if !found {
-			filtered = append(filtered, t)
-		}
-	}
-	return
 }
 
 // die calls log.Fatal if err wasn't nil.
